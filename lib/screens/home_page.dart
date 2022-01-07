e16a7f3b8f2038ed3654577095f01f8a9db510a6
Jogboms <jeremiahogbomo@gmail.com>
2022-01-07 08:44:35 +0100

    First look at home page with grid items

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:rijksbook/data.dart';
+import 'package:rijksbook/domain.dart';
+import 'package:rijksbook/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,9 +11,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final RijksRepository repo = DummyRijksRepository();
+  late final Future<List<Art>> _future = repo.fetchAll(page: 1);
+
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: const Text('RijksBook')),
-        body: const Center(child: Text('Hello world')),
+        body: CustomScrollView(
+          slivers: <Widget>[
+            const SliverAppBar(title: Text('RijksBook'), pinned: true),
+            FutureBuilder<List<Art>>(
+              future: _future,
+              builder: (BuildContext context, AsyncSnapshot<List<Art>> snapshot) {
+                final List<Art>? items = snapshot.data;
+                if (items == null) {
+                  return const SliverToBoxAdapter(child: Center(child: CircularProgressIndicator()));
+                }
+
+                return SliverPadding(
+                  padding: const EdgeInsets.all(8),
+                  sliver: SliverGrid(
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 8,
+                      mainAxisSpacing: 8,
+                    ),
+                    delegate: SliverChildBuilderDelegate(
+                      (_, int index) {
+                        final Art art = items[index];
+                        return ArtGridItem(key: ValueKey<String>(art.id), art: art);
+                      },
+                      childCount: items.length,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
       );
 }
---
+++ lib/widgets.dart
@@ -0,0 +1,1 @@
+export 'widgets/art_grid_item.dart';
