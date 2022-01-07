0244a5d340fceb92cbec227630761a08b05ef195
Jogboms <jeremiahogbomo@gmail.com>
2022-01-07 16:08:25 +0100

    Improve domain models

--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -41,12 +41,15 @@ class ArtDetail {
     required this.id,
     required this.objectNumber,
     required this.colors,
+    required this.title,
     required this.titles,
     required this.longTitle,
     required this.subTitle,
     required this.scLabelLine,
     required this.label,
     required this.description,
+    required this.objectTypes,
+    required this.objectCollection,
     required this.principalMaker,
     required this.principalOrFirstMaker,
     required this.principalMakers,
@@ -59,17 +62,22 @@ class ArtDetail {
     required this.documentation,
     required this.dimensions,
     required this.physicalMedium,
+    required this.webImage,
+    required this.dating,
   });
 
   final String id;
   final String objectNumber;
   final List<ArtColor> colors;
+  final String title;
   final List<String> titles;
   final String longTitle;
   final String subTitle;
   final String scLabelLine;
   final ArtLabel label;
   final String description;
+  final List<String> objectTypes;
+  final List<String> objectCollection;
   final String principalMaker;
   final String principalOrFirstMaker;
   final List<ArtMaker> principalMakers;
@@ -82,6 +90,8 @@ class ArtDetail {
   final List<String> documentation;
   final List<ArtDimension> dimensions;
   final String physicalMedium;
+  final ArtImage webImage;
+  final ArtDating dating;
 
   @override
   bool operator ==(covariant ArtDetail other) =>
@@ -93,7 +103,7 @@ class ArtDetail {
 
   @override
   String toString() =>
-      'ArtDetail{id: $id, objectNumber: $objectNumber, colors: $colors, titles: $titles, longTitle: $longTitle, subTitle: $subTitle, scLabelLine: $scLabelLine, label: $label, description: $description, principalMaker: $principalMaker, principalOrFirstMaker: $principalOrFirstMaker, principalMakers: $principalMakers, plaqueDescriptionEnglish: $plaqueDescriptionEnglish, materials: $materials, productionPlaces: $productionPlaces, hasImage: $hasImage, showImage: $showImage, historicalPersons: $historicalPersons, documentation: $documentation, dimensions: $dimensions, physicalMedium: $physicalMedium}';
+      'ArtDetail{id: $id, objectNumber: $objectNumber, colors: $colors, title: $title, titles: $titles, longTitle: $longTitle, subTitle: $subTitle, scLabelLine: $scLabelLine, label: $label, description: $description, objectTypes: $objectTypes, objectCollection: $objectCollection, principalMaker: $principalMaker, principalOrFirstMaker: $principalOrFirstMaker, principalMakers: $principalMakers, plaqueDescriptionEnglish: $plaqueDescriptionEnglish, materials: $materials, productionPlaces: $productionPlaces, hasImage: $hasImage, showImage: $showImage, historicalPersons: $historicalPersons, documentation: $documentation, dimensions: $dimensions, physicalMedium: $physicalMedium, webImage: $webImage, dating: $dating}';
 }
 
 class ArtImage {
@@ -120,21 +130,27 @@ class ArtMaker {
     required this.name,
     required this.bio,
     required this.unFixedName,
+    required this.nationality,
     required this.placeOfBirth,
     required this.dateOfBirth,
     required this.placeOfDeath,
     required this.dateOfDeath,
     required this.labelDesc,
+    required this.occupation,
+    required this.roles,
   });
 
   final String name;
   final String? bio;
   final String unFixedName;
+  final String nationality;
   final String placeOfBirth;
-  final String dateOfBirth;
-  final String placeOfDeath;
-  final String dateOfDeath;
-  final String labelDesc;
+  final DateTime dateOfBirth;
+  final String? placeOfDeath;
+  final DateTime? dateOfDeath;
+  final String? labelDesc;
+  final List<String> occupation;
+  final List<String> roles;
 
   @override
   bool operator ==(covariant ArtMaker other) =>
@@ -145,31 +161,26 @@ class ArtMaker {
 
   @override
   String toString() =>
-      'ArtMaker{name: $name, bio: $bio, unFixedName: $unFixedName, placeOfBirth: $placeOfBirth, dateOfBirth: $dateOfBirth, placeOfDeath: $placeOfDeath, dateOfDeath: $dateOfDeath, labelDesc: $labelDesc}';
+      'ArtMaker{name: $name, bio: $bio, unFixedName: $unFixedName, nationality: $nationality, placeOfBirth: $placeOfBirth, dateOfBirth: $dateOfBirth, placeOfDeath: $placeOfDeath, dateOfDeath: $dateOfDeath, labelDesc: $labelDesc, occupation: $occupation, roles: $roles}';
 }
 
 class ArtDimension {
-  ArtDimension({required this.unit, required this.type, required this.part, required this.value});
+  ArtDimension({required this.unit, required this.type, required this.value});
 
   final String unit;
   final String type;
-  final String? part;
-  final String value;
+  final double value;
 
   @override
   bool operator ==(covariant ArtDimension other) =>
       identical(this, other) ||
-      runtimeType == other.runtimeType &&
-          unit == other.unit &&
-          type == other.type &&
-          part == other.part &&
-          value == other.value;
+      runtimeType == other.runtimeType && unit == other.unit && type == other.type && value == other.value;
 
   @override
-  int get hashCode => unit.hashCode ^ type.hashCode ^ part.hashCode ^ value.hashCode;
+  int get hashCode => unit.hashCode ^ type.hashCode ^ value.hashCode;
 
   @override
-  String toString() => 'ArtDimension{unit: $unit, type: $type, part: $part, value: $value}';
+  String toString() => 'ArtDimension{unit: $unit, type: $type, value: $value}';
 }
 
 class ArtColor {
@@ -189,6 +200,41 @@ class ArtColor {
   String toString() => 'ArtColor{percentage: $percentage, hex: $hex}';
 }
 
+class ArtDating {
+  ArtDating({
+    required this.presentingDate,
+    required this.sortingDate,
+    required this.period,
+    required this.yearEarly,
+    required this.yearLate,
+  });
+
+  final int presentingDate;
+  final int sortingDate;
+  final int period;
+  final int yearEarly;
+  final int yearLate;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ArtDating &&
+          runtimeType == other.runtimeType &&
+          presentingDate == other.presentingDate &&
+          sortingDate == other.sortingDate &&
+          period == other.period &&
+          yearEarly == other.yearEarly &&
+          yearLate == other.yearLate;
+
+  @override
+  int get hashCode =>
+      presentingDate.hashCode ^ sortingDate.hashCode ^ period.hashCode ^ yearEarly.hashCode ^ yearLate.hashCode;
+
+  @override
+  String toString() =>
+      'ArtDating{presentingDate: $presentingDate, sortingDate: $sortingDate, period: $period, yearEarly: $yearEarly, yearLate: $yearLate}';
+}
+
 class ArtLabel {
   ArtLabel({
     required this.title,
@@ -202,7 +248,7 @@ class ArtLabel {
   final String makerLine;
   final String description;
   final String notes;
-  final String date;
+  final DateTime date;
 
   @override
   bool operator ==(covariant ArtLabel other) =>
